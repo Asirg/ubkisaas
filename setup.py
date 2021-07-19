6faf61e51c95dde11e8aeef172b37a0594e8626c
Asirg <vojt.tieg295i@gmail.com>
2021-07-19 10:32:36 +0300

    fix setuo

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="ubkisaas",
-    version="0.0.1",
+    version="0.0.4",
     author="Asir Muminov",
     author_email="vojt.tieg295i@gmail.com",
     description="library for receiving UBKI data from the official website and parsing them and getting features for scoring analysis",
@@ -17,8 +17,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "ubki"},
-    packages=setuptools.find_packages(where="ubki"),
+    # package_dir={"": "ubki"},
+    packages=find_packages(),
     python_requires=">=3.6",
     install_requires=[
         "xmltodict",
